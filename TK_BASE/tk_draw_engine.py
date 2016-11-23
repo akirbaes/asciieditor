35be1372e2f86ec15ef0eaa473d3d6f282c20c82
Akira BAES <akirbaes@ulb.ac.be>
2016-11-23 12:52:44 +0100

    Ajout du panneau Tools et Colors, mais pas encore de fonctionalité dedans

--- TK_BASE/tk_draw_engine.py
+++ TK_BASE/tk_draw_engine.py
@@ -22,11 +22,23 @@ monofont = font.Font(family="Ubuntu Mono",size = -fh)
 fw = monofont.measure(" ")
 FH=fh
 FW=fw
+DRAWABLE_CHARACTERS = u"""☺☻♥♦♣♠•○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂ ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿıƒ‗─│┌┐└┘├┤┬┴┼═║╔╗╚╝╠╣╦╩╬▀▄█░▒▓■   """
 
 cw = 80
 CW = cw
 ch = 24
 CH = ch
+
+#black,red,green,yellow,blue,purple,cyan,white
+#light with 1
+
+colors = ["black","red","green","gold","blue","purple","cyan","gainsboro"]
+colors2 = ["dark slate gray","tomato","chartreuse","yellow","royal blue","magenta","aquamarine","snow",]
+
+
+curchar = ""
+curfg = "" #16
+curbg = "" #8
 
 class App:
 	def __init__(self,master):
@@ -34,7 +46,7 @@ class App:
 		#self.dupeImage = Tk.PhotoImage(file=os.path.join("Darpteam","Code","GUIELEM","duplicate.gif"))
 		#self.dupeImage = Tk.PhotoImage(file=os.path.join("..","GUIELEM","duplicate.gif"))
 
-		
+		self.allTools = []
 		#MAINFRAME
 		mainFrame = Tk.Frame(master)
 		mainFrame.pack(fill=Tk.BOTH,expand=True)
@@ -47,7 +59,11 @@ class App:
 		
 		self.charactersFrame = Tk.Frame(self.parametersFrame)
 		self.charactersFrame.pack(side=Tk.TOP,anchor="n")
+		self.setCharacters(self.charactersFrame)
 		
+		self.toolsFrame = Tk.Frame(mainFrame)
+		self.toolsFrame.pack(side=Tk.TOP)
+		self.setTools(self.toolsFrame)
 
 		self.displayFrame = Tk.Frame(mainFrame)
 		self.displayFrame.pack(side=Tk.LEFT)
@@ -61,6 +77,42 @@ class App:
 		self.drawing_area = drawingArea(self.canvas_drawing)
 		self.canvas_drawing.addListerner(self.drawing_area)
 		
+	def setTools(self,toolsFrame):
+		self.drawTool = Tk.Button(toolsFrame,text="Draw")
+		self.drawTool.pack(side=Tk.LEFT,anchor = "w")
+		self.allTools.append(self.drawTool)
+		
+		#Action: check if right tool otherwise unpressed
+	
+	def setCharacters(self,charactersFrame):
+		label = Tk.Label(charactersFrame,text="Foreground")
+		label.pack(side=Tk.TOP)
+		fg1 = Tk.Frame(charactersFrame)
+		fg1.pack(side=Tk.TOP)
+		for c in colors:
+			b = Tk.Button(fg1,background=c)
+			b.pack(side=Tk.LEFT)
+		fg2 = Tk.Frame(charactersFrame)
+		fg2.pack(side=Tk.TOP)
+		for c in colors2:
+			b = Tk.Button(fg2,background=c)
+			b.pack(side=Tk.LEFT)
+		
+		label = Tk.Label(charactersFrame,text="Background")
+		label.pack(side=Tk.TOP)
+		bg = Tk.Frame(charactersFrame)
+		bg.pack(side=Tk.TOP)
+		for c in colors:
+			b = Tk.Button(bg,background=c)
+			b.pack(side=Tk.LEFT)
+			
+		label = Tk.Label(charactersFrame,text="Colors")
+		label.pack(side=Tk.TOP)
+		col = Tk.Frame(charactersFrame)
+		col.pack(side=Tk.TOP)
+		for i,c in enumerate(DRAWABLE_CHARACTERS):
+			l = Tk.Label(col,text=c)
+			l.grid(column=int(i%16),row=int(i/16))
 
 
 class canvasManager():
