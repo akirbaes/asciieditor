7180a1374618aa2d8f81dddb66b414a3b8e39cf0
akirbaes <akirbaes@ulb.ac.be>
2017-01-01 15:06:17 +0100

    Using original colors from old project

--- TK_BASE/tk_draw_engine.py
+++ TK_BASE/tk_draw_engine.py
@@ -56,16 +56,36 @@ CW = cw
 ch = 24
 CH = ch
 
+def tocolor(r,g,b):
+	return "#"+(hex(256*256*r+256*g+b)[2:].zfill(6))
 #black,red,green,yellow,blue,purple,cyan,white
 #light with 1
+BLACK = tocolor(0,0,0)
+LTBLACK = tocolor(128,128,128)
+RED = tocolor(128,0,0)
+LTRED = tocolor(255,0,0)
+GREEN = tocolor(0,128,0)
+LTGREEN = tocolor(0,255,0)
+YELLOW = tocolor(128,128,0)
+LTYELLOW = tocolor(255,255,0)
+BLUE = tocolor(0,0,128)
+LTBLUE = tocolor(0,0,255)
+MAGENTA = tocolor(128,0,128)
+LTMAGENTA = tocolor(255,0,255)
+CYAN = tocolor(0,128,128)
+LTCYAN = tocolor(0,255,255)
+WHITE = tocolor(192,192,192)
+LTWHITE = tocolor(224,224,224)
 
 colors = ["black","red","green","gold","blue","purple","cyan","gainsboro"]
 colors2 = ["dark slate gray","tomato","chartreuse","yellow","royal blue","magenta","aquamarine","snow",]
 
+colors = [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE]
+colors2 = [LTBLACK, LTRED, LTGREEN, LTYELLOW, LTBLUE, LTMAGENTA, LTCYAN, LTWHITE]
 
 curchar = " "
-curfg = "orange" #16
-curbg = "tomato" #8
+curfg = RED #"orange" #16
+curbg = CYAN #"tomato" #8
 
 tool = "Linebits"
 
